8f1e4601bad6edb4db6727c55a061e085623c182
Thiago Pereira <thiago.pereira@onearth.com.br>
2023-03-31 07:47:00 -0300

    feat: Ler os arquivos do diretório

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Executou!')
+import * as fs from 'fs'
+
+const rootDir = __dirname.split('/src')[0]
+const businessModel = 'business_model'
+
+function onError(error: unknown) {
+  JSON.stringify(error, null, 2);
+}
+
+function main() {
+  fs.readdir(`${rootDir}/${businessModel}`, (error, filenames) => {
+    if (error) {
+      onError(error);
+      return
+    }
+
+    filenames.forEach(filename => {
+      console.log('filename: ', filename)
+    })
+  })
+}
+
+main();
